f10f630ab7357bbeea4e1680bf81365da860a15f
Vincent Dizon <workvpdizon@gmail.com>
2023-05-12 15:05:26 +0800

    update navbar

--- app/modules/nav/index.tsx
+++ app/modules/nav/index.tsx
@@ -1,7 +1,19 @@
-import React from "react";
-import NavLayout from "./layout";
+import Image from "next/image";
+import { RiMenuFill } from "react-icons/ri";
+import Basket from "./assets/basket.svg";
+import Logo from "./assets/logo.svg";
+import styles from "./nav.module.scss";
+
 const Nav = () => {
-  return <NavLayout>xxxsad</NavLayout>;
+  return (
+    <nav className={styles.root}>
+      <div>
+        <RiMenuFill className={styles.burger} size={25} />
+        <Image src={Logo} height={30} width={30} alt="Sneak logo" />
+      </div>
+      <Image src={Basket} height={30} width={30} alt="Basket" />
+    </nav>
+  );
 };
 
 export default Nav;
--- app/modules/nav/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { ChildrenType } from "@/app/types";
-import styles from "./layout.module.scss";
-
-const NavLayout = ({ children }: ChildrenType) => {
-  return <nav className={styles.root}>{children}</nav>;
-};
-
-export default NavLayout;
