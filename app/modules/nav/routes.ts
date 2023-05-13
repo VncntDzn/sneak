6e004e6b44aa543d79c405a454d041a7f8a34bd5
Vincent Dizon <workvpdizon@gmail.com>
2023-05-13 11:58:15 +0800

    added routes

---
+++ app/modules/nav/routes.ts
@@ -0,0 +1,16 @@
+const routes = [
+  {
+    name: "Home",
+    path: "/home",
+  },
+  {
+    name: "Best Sellers",
+    path: "/best-sellers",
+  },
+  {
+    name: "Categories",
+    path: "/categories",
+  },
+];
+
+export default routes;
