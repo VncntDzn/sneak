629f98c73f83e731e923e85ffa539f48f95502c8
Vincent Dizon <workvpdizon@gmail.com>
2023-05-17 15:46:22 +0800

    update styles for mobile view

--- app/modules/home/hero/index.tsx
+++ app/modules/home/hero/index.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Nike1 from "./assets/Nike1.png";
+import Ellipse from "./assets/ellipse.svg";
 import styles from "./hero.module.scss";
 
 const Hero = () => {
@@ -5,6 +8,14 @@ const Hero = () => {
     <div className={styles.root}>
       <strong className={styles.nikeDunk}>NIKE DUNK</strong>
       <strong className={styles.highRetro}>HIGH RETRO</strong>
+      <div className={styles.heroContainer}>
+        <Image src={Ellipse} alt="Ellipse" height={250} />
+        <Image className={styles.heroImage} src={Nike1} alt="Nike" />
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.buyNowBtn}>Buy Now</button>
+        <button className={styles.detailsBtn}>Details</button>
+      </div>
     </div>
   );
 };
