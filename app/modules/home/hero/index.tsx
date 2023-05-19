86cfc4e66efd73d7edfdb0d54c65dcbf06aeac78
Vincent Dizon <workvpdizon@gmail.com>
2023-05-19 13:27:09 +0800

    added hero layout

--- app/modules/home/hero/index.tsx
+++ app/modules/home/hero/index.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
-import Nike1 from "./assets/Nike1.png";
+import Nike1 from "./assets/ND_HighRetro.png";
 import Ellipse from "./assets/ellipse.svg";
 import styles from "./hero.module.scss";
 
 const Hero = () => {
   return (
     <div className={styles.root}>
-      <strong className={styles.nikeDunk}>NIKE DUNK</strong>
-      <strong className={styles.highRetro}>HIGH RETRO</strong>
+      <div className={styles.textContainer}>
+        <strong className={styles.nikeDunk}>NIKE DUNK</strong>
+        <strong className={styles.highRetro}>HIGH RETRO</strong>
+      </div>
       <div className={styles.heroContainer}>
-        <Image src={Ellipse} alt="Ellipse" height={250} />
-        <Image className={styles.heroImage} src={Nike1} alt="Nike" />
+        <Image className={styles.ellipse} src={Ellipse} alt="Ellipse"/>
+        <Image
+          className={styles.nikeImage}
+          quality={100}
+          src={Nike1}
+          alt="Nike"
+        />
       </div>
       <div className={styles.buttons}>
         <button className={styles.buyNowBtn}>Buy Now</button>
